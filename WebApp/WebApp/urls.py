cff0419b76ed60b32bdc77ea794d796090f75f3d
Carl Castello <carlcastello@yahoo.com>
2017-07-03 23:10:46 -0600

    modified gitignore

--- WebApp/WebApp/urls.py
+++ WebApp/WebApp/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^app/', include('DoSomething.urls', namespace = 'DoSomething')),
+    url(r'', include('DoSomething.urls', namespace = 'DoSomething')),
 ]
